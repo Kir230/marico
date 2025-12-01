3ba537c9b99c23fe60b1b8197850ef91b9c8ac10
KENDAIS <vilegzhaninkirill17@gmail.com>
2025-12-01 22:24:47 +0300

    modified:   upload_header.js 	modified:   ../index.html 	modified:   ../login.html 	deleted:    ../other-style.css 	deleted:    ../style 	deleted:    ../styles/style-footer.css
    	../styles/other-style.css

--- scripts/upload_header.js
+++ scripts/upload_header.js
@@ -1,17 +1,48 @@
-document.writeln(`
-    <header id="header">
-    <img src="images/company-logo.png" alt="" width="196px" height="56px">
-            <nav id="nav">
-                <div id="nav-links">
-                    <a class="nav-link">Use Cases
-                        <img src="down-arrow.png" alt="" width="11px" height="6px">
-                    </a>
-                    <a class="nav-link">About</a>
-                    <a class="nav-link" target="_self" href="pricing.html">Pricing</a>
-                    <a class="nav-link">Blog</a>
-                </div>
-            </nav>
-            <a id="login-link" href="login.html" target="_self">Login</a>
-    <button id="header-button">Sign Up</button>
-</header>`
-);
+let header = document.createElement("header")
+let headerLogo = document.createElement("img")
+let headerNav = document.createElement("nav")
+
+let navBlock = document.createElement("div")
+
+let downArrow = document.createElement("img")
+let loginLink = document.createElement("a")
+let button = document.createElement("button")
+
+button.id = 'header-button'
+button.innerHTML = 'Sign Up'
+
+loginLink.href = 'login.html'
+loginLink.target = '_self'
+loginLink.innerHTML = 'Login'
+
+loginLink.id = 'login-link'
+header.id = 'header'
+headerNav.id = 'nav'
+navBlock.id = 'nav-links'
+
+downArrow.src = 'icons/down-arrow.png'
+downArrow.style.width = '11px'
+downArrow.style.height = '6px'
+
+headerLogo.src = 'images/company-logo.png'
+headerLogo.style.width = '196px'
+headerLogo.style.height = '56px'
+
+let nameLinks = ["Use Cases", "About", "Pricing", "Blog"]
+
+for(x in nameLinks)
+{
+    let links = document.createElement("a");
+    links.className = 'nav-link'
+    links.innerHTML = nameLinks[x]
+    navBlock.append(links)
+}
+
+navBlock.getElementsByClassName('nav-link')[0].append(downArrow)
+header.append(headerLogo)
+headerNav.append(navBlock)
+header.append(headerNav)
+header.append(loginLink)
+header.append(button)
+
+document.body.appendChild(header)
