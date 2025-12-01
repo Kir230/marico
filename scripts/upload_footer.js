f8c4afb0046d0a64ab8ae76ebf35dfa222181dd4
KENDAIS <vilegzhaninkirill17@gmail.com>
2025-12-01 22:16:11 +0300

    modified:   ../index.html 	modified:   ../login.html 	modified:   upload_header.js

--- scripts/upload_footer.js
+++ scripts/upload_footer.js
@@ -1,11 +1,34 @@
-document.writeln(`
-    <footer id="footer">
-        <img src="images/fooLogo.png" alt="" height="105px" width="223px">
-        <nav id="footer_nav">
-                <a class="nav-link">About</a>
-                <a class="nav-link">Pricing</a>
-                <a class="nav-link">Blog</a>
-                <a class="nav-link">Sign in</a>
-        </nav>
-    </footer>
-`)
+
+var footer = document.createElement("footer");
+var image = document.createElement("img");
+var navigation = document.createElement("nav");
+
+let name_links = ["About", "Pricing", "Blog", "Sign in"]
+
+for(x in name_links)
+{
+    let navLink = document.createElement("a")
+    navLink.className = 'nav-link'
+    navLink.innerHTML = name_links[x]
+    navigation.appendChild(navLink)
+}
+
+image.src = 'images/fooLogo.png';
+image.style.width = '223px'
+image.style.height = '105px'
+
+footer.style.display = 'flex'
+footer.style.marginTop = '173px'
+footer.style.marginBottom = '96px'
+footer.style.alignItems = 'center'
+footer.style.justifyContent = 'space-between'
+
+navigation.style.fontWeight = '500'
+navigation.style.fontSize = '28px'
+navigation.style.width = '476px'
+navigation.style.display = 'inherit'
+navigation.style.justifyContent = 'space-between'
+
+footer.append(image)
+footer.append(navigation)
+document.body.appendChild(footer)
